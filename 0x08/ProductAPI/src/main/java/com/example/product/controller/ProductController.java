5cb3e1a2f2f30c38fe3bf8fbf80b929f90774b4c
Gabrielli Venerando <107216833+gabivenerando@users.noreply.github.com>
2022-08-15 16:36:00 -0300

    Update ProductController.java

--- 0x08/ProductAPI/src/main/java/com/example/product/controller/ProductController.java
+++ 0x08/ProductAPI/src/main/java/com/example/product/controller/ProductController.java
@@ -16,7 +16,7 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    @ApiOperation(value="Envia uma mensagem de boas vindas")
+    @ApiOperation(value="Responsável por retornar uma mensagem de boas vindas.")
     @GetMapping("/welcome")
     public String welcome(){
        return "BEM VINDO À PRODUCT REST API.";
@@ -30,28 +30,28 @@ public class ProductController {
         return productRepository.getAllProducts().toString();
     }
 
-    @ApiOperation(value="Retorna um produto pesquisado")
+    @ApiOperation(value="Responsável por retornar um produto pelo id.")
     @ApiResponse(code = 12, message = "The field id not informed. This information is required")
     @GetMapping("/findProductById/{id}")
     public Product findProductById(Integer id){
         return productRepository.getProductById(id);
     }
 
-    @ApiOperation(value="Adiciona um produto")
-    @ApiResponse(code = 10, message = "Required fields not informed")
+    @ApiOperation(value="Responsável por adicionar um produto.")
+    @ApiResponse(code = 10, message = "Responsável por adicionar um produto.")
     @PostMapping("/addProduct")
     public void addProduct(Product product){
        productRepository.addProduct(product);
     }
 
-    @ApiOperation(value="Atualiza um produto")
+    @ApiOperation(value="Responsável por atualizar um produto.")
     @ApiResponse(code = 14, message = "No information has been updated. The new information is the same as recorded in the database")
     @PutMapping("/addProduct")
     public void updateProduct(Product product){
         productRepository.updateProduct(product);
     }
 
-    @ApiOperation(value="Deleta um produto")
+    @ApiOperation(value="Responsável por remover um produto.")
     @ApiResponse(code = 13, message = "User not allowed to remove a product from this category")
     @DeleteMapping("/removeProduct")
     public void removeProduct(Product product){
